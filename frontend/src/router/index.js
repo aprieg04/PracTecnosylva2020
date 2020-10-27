095d3a1928c1995f88b325e1028fd7b96b7e1e65
Adrián Prieto González <aprieg04@github.com>
2020-10-27 08:52:59 +0100

    Plantilla de login
    Plantilla base que será utilizada para el login de la aplicación

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,27 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import App from '../App.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+export default new VueRouter({
+  routes: [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+      path: '/',
+      name: 'App',
+      component: App
+    }
 
-const router = new VueRouter({
-  routes
+  ],
+  mode: "history"
 })
 
-export default router

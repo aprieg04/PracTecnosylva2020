7629150d4acb24ff2c53b5eb7999de560c8ec9be
msperl00 <msperl00@estudiantes.unileon.es>
2020-11-20 09:40:00 +0100

    Inicio sidebar

--- frontend/src/views/Principal.vue
+++ frontend/src/views/Principal.vue
@@ -1,16 +1,24 @@
 <template>
     <v-app>
    <header-component></header-component>
+   <side-bar>
+       <ul class="sidebar-panel-nav">
+       <li><a href="#home">Home</a></li>
+       <li><a href="#about">About</a></li>
+       <li><a href="#contact">Contact</a></li>
+     </ul>
+   </side-bar>
     </v-app>
 </template>
 
 <script>
 
 import HeaderComponent from "../views/HeaderComponent";
+import SideBar from '../views/SideBar.vue';
 
 
 export default {
-  components: { HeaderComponent },
+  components: { HeaderComponent,SideBar },
 name: 'Principal'
 }
 </script>
---
+++ frontend/src/views/SideBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sidebar"></div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  data() {
+    return {
+      
+};
+  }
+}
+</script>
+
+<style>
+@import "../assets/css/styles.css";
+</style>

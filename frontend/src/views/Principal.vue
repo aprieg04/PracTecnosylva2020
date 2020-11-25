f6f0c2ac19fa128262cde165500ad159771dea51
msperl00 <msperl00@estudiantes.unileon.es>
2020-11-25 12:02:25 +0100

    Fin del pase del usuario

--- frontend/src/views/Principal.vue
+++ frontend/src/views/Principal.vue
@@ -1,23 +1,33 @@
 <template>
   <v-app>
-    <header-component username="Marco"></header-component>
+    <header-component :username="username"></header-component>
     <side-bar></side-bar>
-    <div>{{$route.params.id}}</div>
+    
   </v-app>
 </template>∫
-
 <script>
 import HeaderComponent from "../views/HeaderComponent.vue";
 import SideBar from "../views/SideBar.vue";
 
-export default {
+export default { 
+
   components: { HeaderComponent, SideBar },
   name: "Principal",
-  
-  
+  data:
+   function(){return {username: ""}},
+   /* El mounted, hace que podamos pasarlo a la variable. */
+   mounted(){
+     this.username=this.$route.params.id;
+     
+   }
 };
 </script>
 
 <style >
 @import "../assets/css/styles.css";
 </style>
+
+/* Pasamos mediante props del vue router, una url con el nombre de usuario
+el cual corresponde con el id que que hemos guardado en el data. 
+Depues pasamos el username del data, a través del v-bind a la prop del 
+hijo <HeaderCOmponent> */

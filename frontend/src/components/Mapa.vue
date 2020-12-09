8024c3d1f0f2d1a1c9e7997a41d98b37fb653cc1
msperl00 <msperl00@estudiantes.unileon.es>
2020-12-09 10:59:40 +0100

    Prueba de la capa

--- frontend/src/components/Mapa.vue
+++ frontend/src/components/Mapa.vue
@@ -2,20 +2,39 @@
     <div id='mapid'></div>
 </template>
 
+
+    
 <script>
 import leaflet from 'leaflet';
 export default {
     mounted () {
-        var mymap = leaflet.map('mapid').setView([42.59, -5.57], 15);
-        console.log(mymap);
-        leaflet.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
-        maxZoom: 18
-        }).addTo(mymap);
-        leaflet.control.scale().addTo(mymap)
+        var map = leaflet.map('mapid').setView([42.59, -5.57], 15);
+        console.log(map);
+        var basemap = leaflet.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>',
+        zoom:14
+        }).addTo(map);
+        console.log(basemap);
+        leaflet.control.scale().addTo(map);
+ 
+
+ var popup = leaflet.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng) // Sets the geographical point where the popup will open.
+        .setContent("Has hecho click en la coordenada:<br> " +  e.latlng.lat.toString() + "," +  e.latlng.lng.toString()) // Sets the HTML content of the popup.
+        .openOn(map); // Adds the popup to the map and closes the previous one. 
+}
+
+map.on('click', onMapClick);
     },
+
+    
  }
+ 
 </script>
 
 <style>
-  #mapid { height: 100%; width: 100%; border: 1px solid black}
+  #mapid { height: 100%; width: 100%; border: 1px solid black; box-shadow:  20px 20px 50px grey }
 </style>
